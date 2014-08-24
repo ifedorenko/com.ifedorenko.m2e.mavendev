e3877623013bf2cd9f0af3970f62be7a337e4f81
Igor Fedorenko <igor@ifedorenko.com>
2014-08-24 09:18:52 -0400

    fixed workspace resolution disabled when run-as maven junit test
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- com.ifedorenko.m2e.mavendev/src/com/ifedorenko/m2e/mavendev/internal/launching/MavenITLaunchShortcut.java
+++ com.ifedorenko.m2e.mavendev/src/com/ifedorenko/m2e/mavendev/internal/launching/MavenITLaunchShortcut.java
@@ -10,13 +10,28 @@
  *******************************************************************************/
 package com.ifedorenko.m2e.mavendev.internal.launching;
 
+import static org.eclipse.m2e.actions.MavenLaunchConstants.ATTR_WORKSPACE_RESOLUTION;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.junit.launcher.JUnitLaunchShortcut;
 
+@SuppressWarnings( "restriction" )
 public class MavenITLaunchShortcut
     extends JUnitLaunchShortcut
 {
     protected String getLaunchConfigurationTypeId()
     {
         return "com.ifedorenko.m2e.mavendev.itLaunchType";
+    }
+
+    @Override
+    protected ILaunchConfigurationWorkingCopy createLaunchConfiguration( IJavaElement element )
+        throws CoreException
+    {
+        ILaunchConfigurationWorkingCopy configuration = super.createLaunchConfiguration( element );
+        configuration.setAttribute( ATTR_WORKSPACE_RESOLUTION, true );
+        return configuration;
     }
 }
